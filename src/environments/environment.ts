e2cee3206b6e62eaf4b0da6a05f4282d9711a1de
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-01-16 11:22:09 +0700

    angular authentication

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDIUwYhokmfC399LtOYQjb6FpJ_qxdu_Uc",
+    authDomain: "angular-authentication-92cda.firebaseapp.com",
+    projectId: "angular-authentication-92cda",
+    storageBucket: "angular-authentication-92cda.appspot.com",
+    messagingSenderId: "17227979915",
+    appId: "1:17227979915:web:5f4914646a23c59b23088e"
+  }
+};
