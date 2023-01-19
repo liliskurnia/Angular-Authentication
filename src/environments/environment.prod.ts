6d7c7d6516bcabf9e0198d2699b76b05b166e894
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-01-19 15:15:38 +0700

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIUwYhokmfC399LtOYQjb6FpJ_qxdu_Uc",
+    authDomain: "angular-authentication-92cda.firebaseapp.com",
+    projectId: "angular-authentication-92cda",
+    storageBucket: "angular-authentication-92cda.appspot.com",
+    messagingSenderId: "17227979915",
+    appId: "1:17227979915:web:5f4914646a23c59b23088e",
+    databaseURL: "https://angular-authentication-92cda-default-rtdb.firebaseio.com",
+  }
 };
